a7aa4a13854f5103d84552ea76dfef16e1a2ce81
imWillX <imWillX@users.noreply.github.com>
2020-06-26 17:59:41 -0700

    feat(model): Add locations to model, controller, and is outputted in GET request

--- db/seeds/index.js
+++ db/seeds/index.js
@@ -3,6 +3,6 @@
 const Movies = require('./data/movies');
 
 exports.seed = async (Knex) => {
-  await Knex('movies').truncate();
+  // await Knex('movies').truncate();
   await Knex('movies').insert(Movies);
 };
