4f8d53de55250c222203b410e68109bf7dd33c36
William <imWillX@users.noreply.github.com>
2020-06-30 11:41:55 -0700

    feat(movies): GET /movies route (#9)
    * feat(movies): Add ability to retrieve list of movies, filter by exact or range of years, filter by exact of fuzzy title
    
    * test(validator): Update test to use respective validator
    
    * test(movies): Add coverage to new route
    
    * feat(validator): Add check such that year_end is greater than year
    
    * test(validator & controller): Add test to ensure year is less than year_end

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -7,11 +7,60 @@ describe('movie controller', () => {
   describe('create', () => {
 
     it('creates a movie', async () => {
-      const payload = { name: 'Up' };
+      const payload = { name: 'Up', release_year: 2012 };
 
       const movie = await Controller.create(payload);
 
       expect(movie.get('name')).to.eql(payload.name);
+    });
+
+  });
+
+  describe('get', () => {
+
+    it('retrieves all movies', async () => {
+      const query = {};
+
+      const movies = await Controller.get(query);
+
+      expect(movies.length).to.be.above(0);
+    });
+
+    it('retrieves a movie by title', async () => {
+      const query = { name: 'Up' };
+
+      const movies = await Controller.get(query);
+
+      const movie = await movies.fetchOne();
+
+      expect(movie.get('name')).to.eql(query.name);
+    });
+
+    it('retrieves a movie by exact year', async () => {
+      const query = { name: 'Up', release_year: 2012 };
+
+      const movies = await Controller.get(query);
+
+      const movie = await movies.fetchOne();
+
+      expect(movie.get('release_year')).to.eql(query.release_year);
+    });
+
+    it('retrieves a movie by a range', async () => {
+      const query = { release_year: 2012, release_year_end: 2014 };
+
+      const movies = await Controller.get(query);
+
+      const movie = await movies.fetchOne();
+
+      expect(movie.get('release_year')).to.be.within(query.release_year, query.release_year_end);
+    });
+
+    it('release_year_end is less than release_year', async () => {
+      const query = { release_year: 9999, release_year_end: 0 };
+      const movies = await Controller.get(query);
+
+      expect(movies.length).to.eql(0);
     });
 
   });
--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -10,11 +10,26 @@ describe('movies integration', () => {
       const response = await Movies.inject({
         url: '/movies',
         method: 'POST',
-        payload: { name: 'Volver' }
+        payload: { name: 'Volver', release_year: 2014 }
       });
 
       expect(response.statusCode).to.eql(200);
       expect(response.result.object).to.eql('movie');
+    });
+
+    describe('get', () => {
+
+      it('gets a movie', async () => {
+        const response = await Movies.inject({
+          url: '/movies',
+          method: 'GET',
+          payload: {}
+        });
+
+        expect(response.statusCode).to.eql(200);
+        expect(response.result[0].object).to.eql('movie');
+      });
+
     });
 
   });
